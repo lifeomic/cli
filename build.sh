721af0e5bb24fea19730a9f6be70189f79603d5b
Mark Schroering <mark.schroering@lifeomic.com>
2018-06-25 13:26:55 -0400

    Fix browser open for linux

--- build.sh
+++ build.sh
@@ -11,15 +11,8 @@ node_modules/.bin/pkg --target node8-win-x64 --output target/windows-x64/lo.exe 
 node_modules/.bin/pkg --target node8-win-x86 --output target/windows-x86/lo.exe package.json
 node_modules/.bin/pkg --target node8-macos-x64 --output target/macos-x64/lo package.json
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
-(cd target/linux-x64 && zip -q -9 ../lo-${PACKAGE_VERSION}-linux-x64.zip lo)
-(cd target/linux-x86 && zip -q -9 ../lo-${PACKAGE_VERSION}-linux-x86.zip lo)
-(cd target/windows-x64 && zip -q -9 ../lo-${PACKAGE_VERSION}-windows-x64.zip lo.exe)
-(cd target/windows-x86 && zip -q -9 ../lo-${PACKAGE_VERSION}-windows-x86.zip lo.exe)
-(cd target/macos-x64 && zip -q -9 ../lo-${PACKAGE_VERSION}-macos-x64.zip lo)
+(cd target/linux-x64 && zip -q -9 ../lo-linux-x64.zip lo)
+(cd target/linux-x86 && zip -q -9 ../lo-linux-x86.zip lo)
+(cd target/windows-x64 && zip -q -9 ../lo-windows-x64.zip lo.exe)
+(cd target/windows-x86 && zip -q -9 ../lo-windows-x86.zip lo.exe)
+(cd target/macos-x64 && zip -q -9 ../lo-macos-x64.zip lo)
