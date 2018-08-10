945550399f0b84ea56fadc524738631781af3753
Mark Schroering <mark.schroering@lifeomic.com>
2018-08-10 12:40:39 -0400

    checkpoint

--- test/unit/commands/auth.test.js
+++ test/unit/commands/auth.test.js
@@ -7,13 +7,17 @@ const proxyquire = require('proxyquire');
 
 const promptStub = sinon.stub();
 const setStub = sinon.stub();
+let callback;
 
 const program = proxyquire('../../../lib/cmds/auth', {
   'inquirer': {
     prompt: promptStub
   },
   '../config': {
-    set: setStub,
+    set: (key, value) => {
+      setStub(key, value);
+      callback();
+    },
     getEnvironment: () => 'env'
   }
 });
@@ -23,13 +27,16 @@ test.afterEach(t => {
   setStub.resetHistory();
 });
 
-test.serial('The "auth --set" command should update the current access token', async t => {
+test.serial.cb('The "auth --set" command should update the current access token', t => {
   promptStub.returns({
     token: 'token'
   });
 
+  callback = () => {
+    t.true(setStub.calledWith('env.tokens.accessToken', 'token'));
+    t.end();
+  };
+
   yargs.command(program)
     .parse('auth --set');
-
-  t.true(setStub.calledWith('env.tokens.accessToken', 'token'));
 });
