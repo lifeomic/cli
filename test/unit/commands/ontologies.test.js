b6aa5870d90b835339dccaeb0712c17c3fede28d
Taylor Steinberg <taylor.steinberg@lifeomic.com>
2019-04-05 10:15:08 -0400

    Adds a new command 'ontologies' with a single sub-command 'import' for loading data
    Refactors basic for loop into 'for...in' loop

---
+++ test/unit/commands/ontologies.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const yargs = require('yargs');
+const sinon = require('sinon');
+const test = require('ava');
+
+const fs = require('fs');
+
+const proxyquire = require('proxyquire');
+
+let callback;
+
+const stubs = {
+  put: sinon.stub(),
+  stdin: sinon.stub()
+};
+
+const spies = {
+  print: sinon.spy()
+};
+
+const mocks = {
+  '../../api': {
+    put: stubs.put
+  },
+  '../../print': (data, opts) => {
+    spies.print(data, opts);
+    callback();
+  },
+  '../../stdin': () => stubs.stdin()
+};
+
+const _import = proxyquire('../../../lib/cmds/ontologies_cmds/import', mocks);
+
+test.afterEach.always(t => {
+  Object.values(stubs).forEach(stub => stub.resetHistory());
+  Object.values(spies).forEach(spy => spy.resetHistory());
+  callback = null;
+});
+
+test.serial.cb('import', t => {
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  const stdin = fs.createReadStream(`${__dirname}/ontologies/array.json`);
+  stubs.stdin.returns(stdin);
+
+  const data = 'Created';
+  const res = { data: data };
+  stubs.put.onFirstCall().returns(res);
+
+  const project = '9ce94182-f9b6-4043-99c7-420c77c965aa';
+  callback = () => {
+    // Assert that 'PUT' is issued once for each record
+    t.is(1, stubs.put.callCount);
+    t.is(`/v1/terminology/projects/${project}/relationships/`, stubs.put.getCall(0).args[1]);
+
+    // Assert print is invoked once with the HTTP response data
+    t.is(1, spies.print.callCount);
+    t.deepEqual(data, spies.print.getCall(0).args[0]);
+
+    t.end();
+  };
+
+  yargs.command(_import).parse(`import ${project}`);
+});
