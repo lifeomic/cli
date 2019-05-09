0dd07787d0ef546e9d2f70fc6cba27cf0b7775f4
David Taylor <taylorde@regenstrief.org>
2019-05-09 17:56:10 -0400

    PHD-417 Added --jsonLine to make line per resource output and --csv to support a json to CSV conversion via a specification with the mappings of fhir jmesPath to column mapping.

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -x
 rm -rf target
 mkdir -p target
 
-node_modules/.bin/pkg --target node8-linux-x64 --output target/linux-x64/lo package.json
-node_modules/.bin/pkg --target node8-linux-x86 --output target/linux-x86/lo package.json
+#node_modules/.bin/pkg --target node8-linux-x64 --output target/linux-x64/lo package.json
+#node_modules/.bin/pkg --target node8-linux-x86 --output target/linux-x86/lo package.json
 node_modules/.bin/pkg --target node8-win-x64 --output target/windows-x64/lo.exe package.json
-node_modules/.bin/pkg --target node8-win-x86 --output target/windows-x86/lo.exe package.json
-node_modules/.bin/pkg --target node8-macos-x64 --output target/macos-x64/lo package.json
+#node_modules/.bin/pkg --target node8-win-x86 --output target/windows-x86/lo.exe package.json
+#node_modules/.bin/pkg --target node8-macos-x64 --output target/macos-x64/lo package.json
 
-(cd target/linux-x64 && zip -q -9 ../lo-linux-x64.zip lo)
-(cd target/linux-x86 && zip -q -9 ../lo-linux-x86.zip lo)
-(cd target/windows-x64 && zip -q -9 ../lo-windows-x64.zip lo.exe)
-(cd target/windows-x86 && zip -q -9 ../lo-windows-x86.zip lo.exe)
-(cd target/macos-x64 && zip -q -9 ../lo-macos-x64.zip lo)
+#(cd target/linux-x64 && zip -q -9 ../lo-linux-x64.zip lo)
+#(cd target/linux-x86 && zip -q -9 ../lo-linux-x86.zip lo)
+#(cd target/windows-x64 && zip -q -9 ../lo-windows-x64.zip lo.exe)
+#(cd target/windows-x86 && zip -q -9 ../lo-windows-x86.zip lo.exe)
+#(cd target/macos-x64 && zip -q -9 ../lo-macos-x64.zip lo)
