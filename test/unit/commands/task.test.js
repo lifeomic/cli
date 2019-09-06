78f926147c48a4929dfb6833e85735923132c24e
Mark Schroering <mark.schroering@lifeomic.com>
2019-09-06 12:29:39 -0400

    Added Ashion ingest task command

--- test/unit/commands/task.test.js
+++ test/unit/commands/task.test.js
@@ -29,6 +29,7 @@ const list = proxyquire('../../../lib/cmds/tasks_cmds/list', mocks);
 const create = proxyquire('../../../lib/cmds/tasks_cmds/create', mocks);
 const createFoundationTask = proxyquire('../../../lib/cmds/tasks_cmds/create-foundation-task', mocks);
 const createNantomicsTask = proxyquire('../../../lib/cmds/tasks_cmds/create-nantomics-task', mocks);
+const createAshionTask = proxyquire('../../../lib/cmds/tasks_cmds/create-ashion-task', mocks);
 
 test.afterEach.always(t => {
   getStub.resetHistory();
@@ -139,6 +140,28 @@ test.serial.cb('The "create-foundation-xml-import" accepts re-ingest-file as an 
     .parse('create-foundation-xml-import db3e09e9-1ecd-4976-aa5e-70ac7ada0cc3 -x c8ef7300-1373-4e51-8eb9-ff333600f6a5 -r 1234 -s 2a6dc73e-ed30-4387-94c1-0cd661da56d9 -n test3 --test-type test1 --performer-id performer1 --indexed-date "1999-01-01 12:00" --index-type all --re-ingest-file');
 });
 
+test.serial.cb('The "create-ashion-import" command should create a ashion ingest task', t => {
+  const res = { data: {} };
+  postStub.onFirstCall().returns(res);
+  callback = () => {
+    t.is(postStub.callCount, 1);
+    t.is(postStub.getCall(0).args[1], '/v1/tasks/system/ashion-import');
+    t.deepEqual(postStub.getCall(0).args[2], {
+      tarFileId: 'c8ef7300-1373-4e51-8eb9-ff333600f6a5',
+      datasetId: 'db3e09e9-1ecd-4976-aa5e-70ac7ada0cc3',
+      subjectId: '2a6dc73e-ed30-4387-94c1-0cd661da56d9',
+      performerId: 'performer1',
+      indexedDate: '1999-01-01 12:00',
+      outputFilePrefix: 'prefix'
+    });
+    t.is(printSpy.callCount, 1);
+    t.is(printSpy.getCall(0).args[0], res.data);
+    t.end();
+  };
+
+  yargs.command(createAshionTask)
+    .parse('create-ashion-import db3e09e9-1ecd-4976-aa5e-70ac7ada0cc3 -f c8ef7300-1373-4e51-8eb9-ff333600f6a5 -s 2a6dc73e-ed30-4387-94c1-0cd661da56d9 --performer-id performer1 --indexed-date "1999-01-01 12:00" --output-prefix prefix');
+});
 test.serial.cb('The "create-nantomics-vcf-import" command should create a Nantomics ingest task', t => {
   const res = { data: {} };
   postStub.onFirstCall().returns(res);
