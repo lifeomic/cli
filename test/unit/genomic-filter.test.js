fd9a03978f9c1d6634e62db751bc20ccbd047234
Ryan <ryan.ratcliff@lifeomic.com>
2019-09-06 13:36:06 -0400

    Adding in options to pull genomics sets by missing values

--- test/unit/genomic-filter.test.js
+++ test/unit/genomic-filter.test.js
@@ -11,10 +11,8 @@ const genomicFilter = proxyquire(`../../lib/genomic-filter`, {
       }
 
       return {};
-    }
-  },
-  './interceptor/tokenProvider': requestConfig => requestConfig,
-  './config': {
+    },
+
     getEnvironment () { return 'test'; }
   }
 });
