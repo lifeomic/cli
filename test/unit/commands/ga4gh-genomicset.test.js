80d53c287c0ff48bd5fb54be22f473bbb731ec05
Mark Schroering <mark.schroering@lifeomic.com>
2019-12-09 16:48:03 -0500

    Added missing parameters to create genomic set commands

--- test/unit/commands/ga4gh-genomicset.test.js
+++ test/unit/commands/ga4gh-genomicset.test.js
@@ -44,7 +44,8 @@ test.serial.cb('The "ga4gh-genomicsets-create" should create a genomic set', t =
       indexedDate: '1999-01-01 12:00',
       outputVcfName: 'foo.vcf.gz',
       passFilter: false,
-      updateSample: false
+      updateSample: false,
+      testId: undefined
     });
     t.is(printSpy.callCount, 1);
     t.is(printSpy.getCall(0).args[0], res.data);
