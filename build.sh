f8e03ed9d4d0f4c2e1c4a2fe0a9fb88589e3db6f
Chris Hemp <chris.hemp@lifeomic.com>
2020-02-27 12:45:04 -0500

    Improve double-click support on macos.
    - Update `pkg` to latest.
    - Bundle `xdg-open` for linux and macos.
    - Change working directory to `life-export` location when necessary.
    - Remove `open` options hack to make Linux work.

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ node_modules/.bin/pkg --target node12-linux-x64 --output target/linux-x64/life-e
 node_modules/.bin/pkg --target node12-win-x64 --output target/windows-x64/life-export.exe life-export.js
 node_modules/.bin/pkg --target node12-macos-x64 --output target/macos-x64/life-export life-export.js
 
-(cd target/linux-x64 && zip -q -9 ../lo-linux-x64.zip lo life-export)
+cp node_modules/open/xdg-open target/linux-x64/.
+cp node_modules/open/xdg-open target/macos-x64/.
+
+(cd target/linux-x64 && zip -q -9 ../lo-linux-x64.zip lo life-export xdg-open)
 (cd target/windows-x64 && zip -q -9 ../lo-windows-x64.zip lo.exe life-export.exe)
-(cd target/macos-x64 && zip -q -9 ../lo-macos-x64.zip lo life-export)
+(cd target/macos-x64 && zip -q -9 ../lo-macos-x64.zip lo life-export xdg-open)
