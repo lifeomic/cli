49e94ff2a4165046502df1bb9716424979aa355c
mtieman <matthew.tieman@lifeomic.com>
2021-03-12 14:49:10 -0500

    CR-877: Added multi-part upload support for tool-registry-service create command

--- test/unit/commands/tools/tool-test.js
+++ test/unit/commands/tools/tool-test.js
@@ -15,6 +15,7 @@ const delStub = sinon.stub();
 const printSpy = sinon.spy();
 const listStub = sinon.stub();
 const downloadStub = sinon.stub();
+const multipartUploadStub = sinon.stub();
 const axiosMock = new MockAdapter(axios);
 let callback;
 
@@ -34,7 +35,8 @@ const mocks = {
   '../../read': async () => readStub(),
   '../../api': {
     download: downloadStub,
-    get: getApiStub
+    get: getApiStub,
+    multipartUpload: multipartUploadStub
   }
 };
 
@@ -53,7 +55,9 @@ test.always.afterEach(t => {
   listStub.resetHistory();
   downloadStub.resetHistory();
   getApiStub.resetHistory();
+  multipartUploadStub.resetHistory();
   callback = null;
+  delete process.env.TOOL_MULTIPART_MIN_SIZE;
 });
 
 test.serial.cb('The "create" command should create a tool', t => {
@@ -84,6 +88,56 @@ test.serial.cb('The "create" command should create a tool', t => {
 
   yargs.command(create)
     .parse('create -n tool_name  -d "my cli tool" -a ACCOUNT -t Workflow -s 1.5.9 -l "bam,samtools" -f "./test/unit/commands/tools/tool-test.js"');
+});
+
+test.serial.cb('The "create" command should create a tool with multi-part upload', t => {
+  const toolName = 'tool_name';
+  const description = 'docker image';
+  const access = 'ACCOUNT';
+  const type = 'Image';
+  const label = 'test';
+  const uploadId = '00000000-dead-beef-0000-000000000000';
+  const toolId = '00000000-cafe-d00d-0000-000000000000';
+  const version = '0.0.0';
+  const fileName = 'file1.txt';
+
+  const createToolData = {
+    name: toolName,
+    description: description,
+    access: access,
+    version: version,
+    toolClassId: '11',
+    descriptorType: type.toUpperCase(),
+    labels: [label]
+  };
+
+  const requestIdData = {
+    fileName: fileName,
+    toolId: toolId,
+    version: version
+  };
+
+  process.env.TOOL_MULTIPART_MIN_SIZE = 3;
+
+  // create the tool
+  const res = { data: { id: toolId, meta_version: version } };
+  postStub.onCall(0).returns(res);
+
+  // requset the upload id
+  const resUpload = { data: { uploadId: uploadId } };
+  postStub.onCall(1).returns(resUpload);
+
+  callback = () => {
+    t.is(postStub.callCount, 2);
+    t.is(postStub.getCall(0).args[1], '/trs/v2/tools');
+    t.deepEqual(postStub.getCall(0).args[2], createToolData);
+    t.is(postStub.getCall(1).args[1], '/trs/uploads');
+    t.deepEqual(postStub.getCall(1).args[2], requestIdData);
+    t.end();
+  };
+
+  yargs.command(create)
+    .parse(`create -n ${toolName}  -d "${description}" -a ${access} -t ${type} -s ${version} -l "${label}" -f ./test/unit/commands/data/${fileName}`);
 });
 
 test.serial.cb('The "get" should get a tool', t => {
